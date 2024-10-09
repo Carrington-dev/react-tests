2c3a031e8367f4738e9da47914588e9687e045a9
Carrington-dev <crn96m@gmail.com>
2024-10-09 09:28:18 +0200

    Testing default value

--- first_project/src/components/InputField.test.js
+++ first_project/src/components/InputField.test.js
@@ -3,11 +3,21 @@
 import { render, screen } from "@testing-library/react"
 import InputField from "./InputField"
 
-test("Testing input types", () => {
+test("Testing input placeholders", () => {
     render( <InputField 
             type="email" 
             placeholder="Enter your email"
             />)
     const linkElement = screen.getByPlaceholderText("Enter your email")
+    expect(linkElement).toBeInTheDocument()
+})
+
+test("Testing input types", () => {
+    render( <InputField 
+            type="email" 
+            placeholder="Enter your email"
+            defaultValue="email"
+            />)
+    const linkElement = screen.getByDisplayValue("email")
     expect(linkElement).toBeInTheDocument()
 })

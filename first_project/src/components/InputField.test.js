8541c1e541766e21963ed7a1646324aa6e93ff0c
Carrington-dev <crn96m@gmail.com>
2024-10-09 09:32:38 +0200

    Learnt how to group tests

--- first_project/src/components/InputField.test.js
+++ first_project/src/components/InputField.test.js
@@ -3,21 +3,24 @@
 import { render, screen } from "@testing-library/react"
 import InputField from "./InputField"
 
-test("Testing input placeholders", () => {
-    render( <InputField 
-            type="email" 
-            placeholder="Enter your email"
-            />)
-    const linkElement = screen.getByPlaceholderText("Enter your email")
-    expect(linkElement).toBeInTheDocument()
-})
-
-test("Testing input types", () => {
-    render( <InputField 
-            type="email" 
-            placeholder="Enter your email"
-            defaultValue="email"
-            />)
-    const linkElement = screen.getByDisplayValue("email")
-    expect(linkElement).toBeInTheDocument()
+describe("Testing InputField Component", () => {
+    
+    test("Testing input placeholders", () => {
+        render( <InputField 
+                type="email" 
+                placeholder="Enter your email"
+                />)
+        const linkElement = screen.getByPlaceholderText("Enter your email")
+        expect(linkElement).toBeInTheDocument()
+    })
+    
+    test("Testing input types", () => {
+        render( <InputField 
+                type="email" 
+                placeholder="Enter your email"
+                defaultValue="email"
+                />)
+        const linkElement = screen.getByDisplayValue("email")
+        expect(linkElement).toBeInTheDocument()
+    })
 })
